60a5bfe49e0f11cfc4d5ff2addd17043d219ddf2
Alex Ellis <alexellis2@gmail.com>
2017-12-23 16:01:48 +0000

    ServiceQuery interface
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gateway/plugin/external.go
+++ gateway/plugin/external.go
@@ -21,12 +21,6 @@ import (
 	"github.com/openfaas/faas/gateway/requests"
 )
 
-// ServiceQuery provides interface for replica querying/setting
-type ServiceQuery interface {
-	GetReplicas(service string) (currentReplicas uint64, maxReplicas uint64, minReplicas uint64, err error)
-	SetReplicas(service string, count uint64) error
-}
-
 // NewExternalServiceQuery proxies service queries to external plugin via HTTP
 func NewExternalServiceQuery(externalURL url.URL) handlers.ServiceQuery {
 	proxyClient := http.Client{
